74b017485e757968cc2586df5006229b087c7885
User <User>
2021-10-30 17:34:54 +0300

    123

--- ping_pong.py
+++ ping_pong.py
@@ -1,9 +1,9 @@
 import pygame
+import random
 pygame.init()
-from random import uniform
-#123
-#123
 
+#Koment
+#123
 mw_w = 500
 mw_h = 500
 
@@ -12,8 +12,8 @@ mw.fill((0,255,0))
 clock = pygame.time.Clock()
 FPS = 60
 
-class Sprite:
-    def __init__(self, image_name, x, y, width, hight, speed):
+class Sprite():
+    def __init__(self, image_name, x, y, width, hight, speed = 4):
         self.image = pygame.transform.scale(
             pygame.image.load(image_name), (width, hight))
         self.rect = self.image.get_rect()
@@ -25,14 +25,6 @@ class Sprite:
         mw.blit(self.image, (self.rect.x, self.rect.y))
 
 class Rockets(Sprite):
-    #def init(self, image_name, x, y, width,
-        #hight, speed, ball_speed1, ball_speed2):
-        #super().init(self, image_name, x, y, width, hight, speed)
-        #self.b_speed1 = ball_speed1
-        #self.b_speed2 = ball_speed2
-    def instant_(self):
-        self.speed_x = 3
-        self.speed_y = 3
     def rocket_l(self):
         key_ = pygame.key.get_pressed()
         if key_[pygame.K_w] and self.rect.y > 0:
@@ -47,61 +39,74 @@ class Rockets(Sprite):
         if key_[pygame.K_DOWN] and self.rect.y < mw_h - 106:
             self.rect.y += self.speed
 
+class Ball(Sprite):
+
+    def inst_speed(self):
+        self.Speed_x = self.speed
+        self.Speed_y = self.speed
+    
+    def pong_x(self):
+        if self.rect.x > mw_w - 26:
+            self.Speed_x = random.uniform(3.5, 5.5) * -1
+        elif self.rect.x < 0:
+            self.Speed_x = random.uniform(3.0, 5.5)
+
     def move(self):
-        speed1 = 3
-        speed2 = 3
-        self.rect.x += ball.speed_x
-        self.rect.y += ball.speed_y
-        if self.rect.x > 500:
-            speed1 *= -1
+        self.rect.x += self.Speed_x
+        self.rect.y += self.Speed_y
 
-        if self.rect.y > mw_h - 26 or self.rect.y < 0:
-            speed2 *= -1
+        if self.rect.y > mw_h - 26:
+            self.Speed_y = random.uniform(3.0, 5.5) * -1
+        elif self.rect.y < 0:
+            self.Speed_y = random.uniform(3.0, 5.5)
 
+rct_l = Rockets('rc.png', 5, 0, 16, 106)
+rct_r = Rockets('rc.png', mw_w - 20, 0, 16, 106)
+ball = Ball('ball.png', random.randint(50,mw_w-50), random.randint(80,mw_w-80),26,26, random.choice([-3,3]))
+ball.inst_speed()
 
-rct_l = Rockets('rc.png', 5, 0, 16, 106, 3)
-rct_r = Rockets('rc.png', mw_w - 20, 0, 16, 106, 3)
-ball = Rockets('ball.png', 220, 4, 26, 26, 5)
-ball.instant_()
+def ping_pong():
+    if pygame.sprite.collide_rect(rct_l, ball):
+        ball.Speed_x = random.uniform(3, 5)
+        ball.Speed_y = random.uniform(-4, 4)
+        '''
+        if random.random() > 0.5:
+            ball.Speed_y *= -1
+        else:
+            ball.Speed_y *= 1
+        '''
+    if pygame.sprite.collide_rect(rct_r, ball):
+        ball.Speed_x = -random.uniform(3, 5)
+        ball.Speed_y = random.uniform(-4, 4)
+        '''
+        if random.random() > 0.5:
+            ball.Speed_y *= -1
+        else:
+            ball.Speed_y *= 1
+        '''
 
 game = True
-speed1 = 3
-speed2 = 3
+
 while game:
 
     for e in pygame.event.get():
         if e.type == pygame.QUIT:
             game = False
 
+    ping_pong()
+
     if game:
         mw.fill((0,255,0))
         rct_l.rocket_l()
         rct_r.rocket_r()
-        #ball.move()
-
-   
-    ball.rect.x += speed1
-    ball.rect.y += speed2
-
-    if ball.rect.x > mw_w - 26 or ball.rect.x < 0:
-        speed1 *= -1
-
-    if ball.rect.y > mw_h - 26 or ball.rect.y < 0:
-        speed2 *= -1
-    
-    if pygame.sprite.collide_rect(rct_l,ball):
-        ball.speed_y = uniform(-5,5)
-        ball.speed_x *= -1
-
-    if pygame.sprite.collide_rect(rct_r,ball):
-        ball.speed_y = uniform(-5,5)
-        ball.speed_x *= -1
+        ball.move()
+        ball.pong_x()
 
     rct_l.reset()
     rct_r.reset()
     ball.reset()
 
-#wwww
-
     pygame.display.update()
     clock.tick(FPS)
+
+
